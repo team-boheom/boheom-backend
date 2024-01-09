fd656f617359c578bd10f09ca882897fa06be5c2
jyk1029 <jyk102999@gmail.com>
2024-01-09 09:58:29 +0900

    set :: project setting

---
+++ src/main/kotlin/com/example/boheom/BoheomApplication.kt
@@ -0,0 +1,11 @@
+package com.example.boheom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoheomApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoheomApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/boheom/BoheomApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.boheom
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoheomApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
