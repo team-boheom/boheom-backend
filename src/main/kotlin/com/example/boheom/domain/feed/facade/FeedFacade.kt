24eb51a8ff6fd60fa51c39c01f8227388f18571f
jyk1029 <jyk102999@gmail.com>
2024-01-18 01:20:48 +0900

    refactor :: use facade pattern

--- src/main/kotlin/com/example/boheom/domain/feed/facade/FeedFacade.kt
+++ src/main/kotlin/com/example/boheom/domain/feed/facade/FeedFacade.kt
@@ -1,16 +1,32 @@
 package com.example.boheom.domain.feed.facade
 
 import com.example.boheom.domain.feed.domain.Feed
+import com.example.boheom.domain.feed.domain.repository.ApplyRepository
 import com.example.boheom.domain.feed.domain.repository.FeedRepository
+import com.example.boheom.domain.feed.domain.repository.FeedTagRepository
 import com.example.boheom.domain.feed.exception.FeedNotFoundException
+import com.example.boheom.domain.feed.presentation.dto.response.FeedElement
+import com.example.boheom.domain.feed.presentation.dto.response.FeedListResponse
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
 class FeedFacade(
     private val feedRepository: FeedRepository,
+    private val feedTagRepository: FeedTagRepository,
+    private val applyRepository: ApplyRepository,
 ) {
     fun getByFeedId(feedId: UUID): Feed {
         return feedRepository.findById(feedId).orElseThrow { FeedNotFoundException }
+    }
+
+    fun getFeedList(feeds: List<Feed>): FeedListResponse {
+        val feedElements = feeds.map { feed ->
+            val tags = feedTagRepository.findAllByFeed(feed)
+            val applyCount = applyRepository.countByFeed(feed)
+
+            FeedElement(feed.id, feed.title, feed.content, feed.view, tags.map { it.name }, feed.recruitment, applyCount)
+        }
+        return FeedListResponse(feedElements)
     }
 }
--- src/main/kotlin/com/example/boheom/domain/feed/service/PopularFeedListService.kt
+++ src/main/kotlin/com/example/boheom/domain/feed/service/PopularFeedListService.kt
@@ -3,6 +3,7 @@ package com.example.boheom.domain.feed.service
 import com.example.boheom.domain.feed.domain.repository.ApplyRepository
 import com.example.boheom.domain.feed.domain.repository.FeedRepository
 import com.example.boheom.domain.feed.domain.repository.FeedTagRepository
+import com.example.boheom.domain.feed.facade.FeedFacade
 import com.example.boheom.domain.feed.presentation.dto.response.FeedElement
 import com.example.boheom.domain.feed.presentation.dto.response.FeedListResponse
 import org.springframework.stereotype.Service
@@ -11,18 +12,11 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class PopularFeedListService(
     private val feedRepository: FeedRepository,
-    private val feedTagRepository: FeedTagRepository,
-    private val applyRepository: ApplyRepository,
+    private val feedFacade: FeedFacade,
 ) {
     @Transactional(readOnly = true)
     fun execute(): FeedListResponse {
         val feeds = feedRepository.findAllByOrderByViewDesc()
-        val feedElements = feeds.map { feed ->
-            val tags = feedTagRepository.findAllByFeed(feed)
-            val applyCount = applyRepository.countByFeed(feed)
-            FeedElement(feed.id, feed.title, feed.content, feed.view, tags.map { it.name }, feed.recruitment, applyCount)
-        }
-
-        return FeedListResponse(feedElements)
+        return feedFacade.getFeedList(feeds)
     }
 }
