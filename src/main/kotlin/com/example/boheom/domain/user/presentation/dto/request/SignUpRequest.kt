bf8238cf19e048ab8f6ba8fa4b358578d1e862a3
jyk1029 <jyk102999@gmail.com>
2024-01-18 00:54:16 +0900

    chore :: regexp

--- src/main/kotlin/com/example/boheom/domain/user/presentation/dto/request/SignUpRequest.kt
+++ src/main/kotlin/com/example/boheom/domain/user/presentation/dto/request/SignUpRequest.kt
@@ -17,7 +17,7 @@ data class SignUpRequest(
     @field:Pattern(
         regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%&'()*+,./:;<=>?@＼^_`{|}~])[a-zA-Z0-9!#$%&'()*+,./:;" +
                 "<=>?@＼^_`{|}~]{8,30}$",
-        message = "password는 소문자, 숫자, 특수문자가 포함되어야 합니다."
+        message = "소문자, 숫자, 특수문자가 포함되어야 하며 5자~30자 사이여야 합니다."
     )
     val password: String,
     val checkPassword: String
